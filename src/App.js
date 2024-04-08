dd70ed26da1de708fc7e5d4a06c635b08718bdbd
Jasper Chan <jchancoding@gmail.com>
2024-04-08 02:10:02 -0700

    Add search and user login
    -added searchUser functionality for /users/user/${username}
    -added dummy user authentication and route protection for authProfile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./components/home";
 import AboutUs from "./components/about";
 import Users from "./components/users";
 import Navbar from './components/navbar';
 import NotFound from "./components/notFound";
+import UserProfile from './components/userProfile';
+import SearchUser from './components/searchUser';
+import AuthProfile from './components/authProfile';
+import Login from "./components/login";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [isLogged, setIsLogged] = useState(false);
+
   return (
     <div className="App">
       <Routes>
@@ -15,6 +22,10 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/users" element={<Users />} />
+          <Route path="/users/user/:username" element={<UserProfile />} />
+          <Route path="/search" element={<SearchUser />} />
+          <Route path="/login"  element={<Login setIsLogged={setIsLogged} setUsername={setUsername}/>} />
+          <Route path="/authprofile" element={isLogged ? <AuthProfile username={username}/> : <Navigate replace to={"/login"} />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
