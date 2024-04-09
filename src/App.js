625c7190e04264db48959a0b998bccd554abab74
Jasper Chan <jchancoding@gmail.com>
2024-04-09 02:12:43 -0700

    LazyLoading
    -changed routes in app.js to use lazy loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import { Route, Routes, Navigate, useLocation } from "react-router-dom";
 import Home from "./components/home";
-import AboutUs from "./components/about";
-import Users from "./components/users";
-import NotFound from "./components/notFound";
-import UserProfile from './components/userProfile';
-import SearchUser from './components/searchUser';
-import AuthProfile from './components/authProfile';
-import Login from "./components/login";
-import { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { SwitchTransition, CSSTransition } from "react-transition-group";
+
+const Users = lazy(() => import('./components/users'))
+const UserProfile = lazy(() => import('./components/userProfile'))
+const SearchUser = lazy(() => import('./components/searchUser'))
+const Login = lazy(() => import('./components/login'))
+const AuthProfile = lazy(() => import('./components/authProfile'))
+const AboutUs = lazy(() => import('./components/about'))
+const NotFound = lazy(() => import('./components/notFound'))
 
 function App() {
   const [username, setUsername] = useState("");
@@ -25,30 +26,32 @@ function App() {
           timeout={300}
           unmountOnExit
         >
-          <Routes Location={location}>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/users/user/:username" element={<UserProfile />} />
-            <Route path="/search" element={<SearchUser />} />
-            <Route
-              path="/login"
-              element={
-                <Login setIsLogged={setIsLogged} setUsername={setUsername} />
-              }
-            />
-            <Route
-              path="/authprofile"
-              element={
-                isLogged ? (
-                  <AuthProfile username={username} />
-                ) : (
-                  <Navigate replace to={"/login"} />
-                )
-              }
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Suspense fallback={() => <h1>Loading...</h1>}>
+            <Routes Location={location}>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/about" element={<AboutUs />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/users/user/:username" element={<UserProfile />} />
+              <Route path="/search" element={<SearchUser />} />
+              <Route
+                path="/login"
+                element={
+                  <Login setIsLogged={setIsLogged} setUsername={setUsername} />
+                }
+              />
+              <Route
+                path="/authprofile"
+                element={
+                  isLogged ? (
+                    <AuthProfile username={username} />
+                  ) : (
+                    <Navigate replace to={"/login"} />
+                  )
+                }
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </CSSTransition>
       </SwitchTransition>
     </div>
